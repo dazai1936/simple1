f8ec8db81bc44a03c019dbfeff6e2756e1d78a7e
sangwoo <whiz10@naver.com>
2024-07-17 16:40:38 +0900

    first commit

--- src/main/java/org/example/simple1/controller/TestController.java
+++ src/main/java/org/example/simple1/controller/TestController.java
@@ -12,7 +12,7 @@ public class TestController {
     public MemberDto test() {
         MemberDto memberDto = new MemberDto();
         memberDto.setName("test");
-        memberDto.setAge(10);
+        memberDto.setAge(20);
         return memberDto;
     }
 }
