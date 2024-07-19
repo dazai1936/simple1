5f52d9a07b6c2ce9ea3b659541d11291d88df055
sangwoo <whiz10@naver.com>
2024-07-19 11:35:40 +0900

    first commit

--- src/main/java/org/example/simple1/controller/TestController.java
+++ src/main/java/org/example/simple1/controller/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
         memberDto.setAge(20);
         return memberDto;
     }
-    @GetMapping("/app2")
+    @GetMapping("/app22")
     public MemberDto test2() {
         MemberDto memberDto = new MemberDto();
         memberDto.setName("test");
